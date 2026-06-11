093b79c361cdb648731318612b725335f48eaedd
Cyril Blum <c@cblum.ch>
2026-06-11 17:01:55 +0200

    fixes for much improved legibility and clarity of big o notation

--- Grundlagen_Info/00_Programmieren/Skript/Code/K06_Datenstrukturen/count_common_elements.py
+++ Grundlagen_Info/00_Programmieren/Skript/Code/K06_Datenstrukturen/count_common_elements.py
@@ -1,11 +1,15 @@
-a = [1, 2, 2, 3, 5]
-b = [2, 2, 4, 5]
+liste_a = [1, 2, 2, 3, 5]
+liste_b = [2, 2, 4, 5]
 
-# zähle gemeinsame elemente
+# zähle gemeinsame Elemente
 count = 0
-for element1 in liste1:
-    for element2 in liste2:
-        if element1 == element2:
+index_a = 0
+for _ in range(len(liste_a)):
+    index_b = 0
+    for _ in range(len(liste_b)):
+        if liste_a[index_a] == liste_b[index_b]:
             count += 1
-            break  # Verhindert, dass das gleiche Element in liste2 mehrfach gezählt wird
-print(count)
+            break  # Verhindert, dass das gleiche Element in liste_b mehrfach gezählt wird
+        index_b += 1
+    index_a += 1
+print(count)    
