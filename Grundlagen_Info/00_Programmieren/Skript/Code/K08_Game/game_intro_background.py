ef532892e69690abda77c1435a316336779b13c7
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2026-08-26 15:12:39 +0200

    corrected mistake in solutions

--- Grundlagen_Info/00_Programmieren/Skript/Code/K08_Game/game_intro_background.py
+++ Grundlagen_Info/00_Programmieren/Skript/Code/K08_Game/game_intro_background.py
@@ -5,9 +5,11 @@ pg.init()  # Pygame initialisieren (starten)
 WINDOW = (800, 600)  # Fenstergrösse (als Tuple gespeichert)
 screen = pg.display.set_mode(WINDOW)  # Fenster erstellen
 pg.display.set_caption("Mein erstes Game")  # Fenstertitel setzen
+clock = pg.time.Clock()  # Clock für Zeitsteuerung erstellen
 icon = pg.image.load("Grundlagen_Info/00_Programmieren/Skript/Code/K09_Game/icon.png")
 pg.display.set_icon(icon)
-clock = pg.time.Clock()  # Clock für Zeitsteuerung erstellen
+
+frame_counter = 0
 
 running = True  # Hauptschleife
 while running:
@@ -17,15 +19,18 @@ while running:
             running = False
 
     # --- render (zeichnen) ---
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    background_color = (r, g, b)
-    screen.fill(background_color)  # Hintergrundfarbe setzen
+    if frame_counter % 10 == 0:
+        # ändere die Farbe bei jedem 10ten Frame
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        background_color = (r, g, b)
+        screen.fill(background_color)  # Hintergrundfarbe setzen
 
     pg.display.flip()
+    frame_counter += 1
 
     # --- Zeitsteuerung ---
-    clock.tick(10)  # 10 FPS
+    clock.tick(60)  # 10 FPS
 
 pg.quit()
